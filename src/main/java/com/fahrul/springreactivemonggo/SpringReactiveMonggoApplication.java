9384158871078fa20ca484985ee7707b0e55c62d
Fahrul Arya Sugita <faryasugita@gmail.com>
2021-07-01 09:58:19 +0700

    istimiwir

---
+++ src/main/java/com/fahrul/springreactivemonggo/SpringReactiveMonggoApplication.java
@@ -0,0 +1,13 @@
+package com.fahrul.springreactivemonggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMonggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveMonggoApplication.class, args);
+	}
+
+}
